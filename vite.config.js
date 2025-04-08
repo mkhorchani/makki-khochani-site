6e82c4b4d362d6ec742ddb9c3fea884f933354c6
MakkiSp <makki.khorchani@gmail.com>
2025-04-08 12:43:52 -0500

    Corrected misspeled last name

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/makki-khochani-site/',
+  base: '/makki-khorchani-site/',
   plugins: [react()],
 });
